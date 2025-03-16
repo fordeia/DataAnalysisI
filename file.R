1d8ce3b7ab4cccb9e62135fcc615eebaf0727342
fordeia <157367221+fordeia@users.noreply.github.com>
2025-03-16 07:42:34 -0400

    update R file combine La Nina and El Nino

--- file.R
+++ file.R
@@ -1,4 +1,7 @@
 CBloom <-read.table("cherry_blossom.txt",header =TRUE,sep="\t", fill = TRUE)
 
+
 # Create a new variable by summing two existing variables
-    my_data$sum_col <- my_data$col1 + my_data$col2
+    CBloom$OceTemp <- 2*CBloom$El.Nino+CBloom$La.Nina
+
+head(CBloom)

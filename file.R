609198bb91dc3774527c0d7bc2824fabe02c9d14
fordeia <157367221+fordeia@users.noreply.github.com>
2025-03-16 09:17:14 -0400

    Update file.R

--- file.R
+++ file.R
@@ -1,5 +1,7 @@
 CBloom <-read.table("cherry_blossom.txt",header =TRUE,sep="\t", fill = TRUE)
 
+#Dropping unwanted variables
+CBloom<-CBloom[ , !(names(CBloom) %in% "X")]
 
 # Create a new variable by summing two existing variables
     CBloom$OceTemp <- 2*CBloom$El_Nino+CBloom$La_Nina

0f756507634e9cfffd5f703263501022615f4da9
fordeia <157367221+fordeia@users.noreply.github.com>
2025-03-20 00:07:15 -0400

    Update SimpleLinearRegression.py

--- SimpleLinearRegression.py
+++ SimpleLinearRegression.py
@@ -1,3 +1,5 @@
 #Simple linear regression in Python
 import numpy as np
 from sklearn.linear_model import LinearRegression
+x = np.array([5, 15, 25, 35, 45, 55]).reshape((-1, 1))
+y = np.array([5, 20, 14, 32, 22, 38])

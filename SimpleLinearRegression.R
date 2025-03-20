3ae48dab5b0ef697c213d5d0b87c41c11807fe22
fordeia <157367221+fordeia@users.noreply.github.com>
2025-03-20 01:14:54 -0400

    Update SimpleLinearRegression.R

--- SimpleLinearRegression.R
+++ SimpleLinearRegression.R
@@ -27,6 +27,7 @@ qqline(residuals(lm.r), col = "steelblue", lwd = 2)
 shapiro.test(residuals(lm.r))
 
 #Making predictions
-predictions <- predict(lm.r, newdata = data$Years_Exp)
+new_data <- data.frame(yearExp = c(1.2, 1.3, 1.6, 2.1, 2.2, 2.9, 3.3, 3.2, 3.5, 3.7))
+predictions <- predict(lm.r, newdata = new_data)
 predictions
 

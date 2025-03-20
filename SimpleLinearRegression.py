7dd65c56120bf28bd0556b635c9a5a13e48fbe63
fordeia <157367221+fordeia@users.noreply.github.com>
2025-03-20 00:13:49 -0400

    Update SimpleLinearRegression.py

--- SimpleLinearRegression.py
+++ SimpleLinearRegression.py
@@ -1,5 +1,12 @@
 #Simple linear regression in Python
 import numpy as np
 from sklearn.linear_model import LinearRegression
-x = np.array([5, 15, 25, 35, 45, 55]).reshape((-1, 1))
-y = np.array([5, 20, 14, 32, 22, 38])
+x = np.array([1.1, 1.3, 1.5, 2.0, 2.2, 2.9, 3.0, 3.2, 3.2, 3.7]).reshape((-1, 1))
+y = np.array([39343.00, 46205.00, 37731.00, 43525.00, 39891.00, 56642.00, 60150.00, 54445.00, 64445.00, 57189.00])
+
+#Fitting the model
+model = LinearRegression().fit(x, y)
+
+#Obtaining R square
+r_sq = model.score(x, y)
+print(f"coefficient of determination: {r_sq}")

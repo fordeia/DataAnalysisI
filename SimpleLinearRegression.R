92ef643818695c392de0a659a0566cef88413f10
fordeia <157367221+fordeia@users.noreply.github.com>
2025-03-20 02:00:07 -0400

    Update SimpleLinearRegression.R

--- SimpleLinearRegression.R
+++ SimpleLinearRegression.R
@@ -19,8 +19,6 @@ plot(data$Years_Exp, data$Salary,
      ylab = "Salary",
      main = "Scatter Plot of Years Experienced vs Salary")
 
-#Testing assumptions of the model
-
 #Testing the normality assumption of the residuals using a Q-Q plot and the Shapiro Wilk test
 qqnorm(residuals(lm.r))
 qqline(residuals(lm.r), col = "steelblue", lwd = 2)
@@ -28,10 +26,14 @@ qqline(residuals(lm.r), col = "steelblue", lwd = 2)
 #Testing the normality assumption of the residuals using the Shapiro-Wilk test
 shapiro.test(residuals(lm.r))
 
-#Testing homoscedasticity
+#Testing homoscedasticity using a residual plot
 plot(lm.r$fitted.values, lm.r$residuals, main = "Residual Plot",
          xlab = "Fitted Values", ylab = "Residuals")
     abline(h=0, col="red") # Add a horizontal line at zero for reference
+
+# Test for homoscedasticity using Breusch-Pagan test
+library(lmtest)
+bptest(lm.r)
 
 #Making predictions
 new_data <- data.frame(Years_Exp = c(1.2, 1.3, 1.6, 2.1, 2.2, 2.9, 3.3, 3.2, 3.5, 3.7))

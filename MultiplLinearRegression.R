b25244f100f7316057a814ab62f012be41753186
fordeia <157367221+fordeia@users.noreply.github.com>
2025-03-23 11:30:12 -0400

    Update MultiplLinearRegression.R

--- MultiplLinearRegression.R
+++ MultiplLinearRegression.R
@@ -1,4 +1,8 @@
+library(tidyverse)
 #Down loading the data
 data("marketing", package = "datarium")
 head(marketing)
 
+#Fitting the model
+model <- lm(sales ~ youtube + facebook + newspaper, data = marketing)
+summary(model)

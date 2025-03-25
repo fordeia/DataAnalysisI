c203bfee4a22f7305eec04e291dab04f73eb7e19
fordeia <157367221+fordeia@users.noreply.github.com>
2025-03-24 22:15:12 -0400

    Update multipleLinearRegression.py

---
+++ multipleLinearRegression.py
@@ -0,0 +1,18 @@
+import statsmodels.api as sm
+import pandas as pd
+
+# Sample data (replace with your actual data)
+data = pd.read_excel(r"C:/Users/fordeia/DataAnalysisI/marketingData.xlsx")
+df = pd.DataFrame(data)
+# Define the dependent and independent variables
+Y = df['sales']
+X = df[['youtube', 'facebook','newspaper']]
+
+# Add a constant to the independent variables (for the intercept)
+X = sm.add_constant(X)
+
+# Fit the multiple linear regression model
+model = sm.OLS(Y, X).fit()
+
+# Print the model summary
+print(model.summary())

f561149fb5b2bc32c59c9cd84d86d2879b2f4e53
fordeia <157367221+fordeia@users.noreply.github.com>
2025-03-24 20:11:50 -0400

    Update MultiplLinearRegression.R

--- MultiplLinearRegression.R
+++ MultiplLinearRegression.R
@@ -5,7 +5,6 @@ head(marketing)
 
 #Fitting the model
 model <- lm(sales ~ youtube + facebook + newspaper, data = marketing)
-
 summary(model)
 
 #Removing the insignificant variable and refitting the model

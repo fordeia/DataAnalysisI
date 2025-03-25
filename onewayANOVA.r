90107625b8f4973e744f487b7f821b333d2f0e80
fordeia <157367221+fordeia@users.noreply.github.com>
2025-03-25 06:07:49 -0400

    Update onewayANOVA.r

--- onewayANOVA.r
+++ onewayANOVA.r
@@ -1,3 +1,6 @@
 #Obtaining Data
 CropData <-read.table("cropData.txt",header =TRUE,sep="\t", fill = TRUE)
 head(CropData)
+
+#Fitting the oneway model
+one.way <- aov(yield ~ fertilizer, data = cropData)

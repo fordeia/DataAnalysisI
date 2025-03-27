a887d60f194f54ab36e69177cabaa811cd84fb5d
fordeia <157367221+fordeia@users.noreply.github.com>
2025-03-27 06:51:55 -0400

    Update MidtermCodes.R

--- MidtermCodes.R
+++ MidtermCodes.R
@@ -13,11 +13,17 @@ mean(Women_bodyfat)
 sd(Women_bodyfat) 
 
 # combine two vectors using cbind 
-# function 
+  if (nrow(Men_bodyfat) < nrow(Women_bodyfat)) {
+        # Pad Men_bodyfat with NA rows
+        Men_bodyfat <- rbind(Men_bodyfat, matrix(NA, nrow = nrow(Women_bodyfat) - nrow(Men_bodyfat), ncol = ncol(Men_bodyfat)))
+    } else if (nrow(Women_bodyfat) < nrow(Men_bodyfat)) {
+        # Pad Women_bodyfat with NA rows
+        Women_bodyfat <- rbind(Women_bodyfat, matrix(NA, nrow = nrow(Men_bodyfat) - nrow(Women_bodyfat), ncol = ncol(Women_bodyfat)))
+    }
 bodyfat_data=cbind(Men_bodyfat,Women_bodyfat) 
   
 # boxplot 
 boxplot(bodyfat_data,beside=T) 
 
-Two sample t-test
+#Two sample t-test
 t.test(Men_bodyfat, Women_bodyfat, var.equal = TRUE)

379b1e170aedd805cb8d1127913e4c07442ac9c1
fordeia <157367221+fordeia@users.noreply.github.com>
2025-03-28 08:51:12 -0400

    Update MidtermCodes.R

--- MidtermCodes.R
+++ MidtermCodes.R
@@ -1,3 +1,10 @@
+# Testing the normality assumption, Q-Q plot
+# read in the data
+x <- c(28,29,35,37,32,26,37,39,22,29,36,38)
+# making the qqplot
+qqnorm(x)
+qqline(x)
+
 #Two sample t test
 Men_bodyfat <- c(13.3,6,20,8,14,19,18,25,16,24,15,1,15)
 Women_bodyfat <- c(22,16,21.7,21,30,12,23.2,28,23)

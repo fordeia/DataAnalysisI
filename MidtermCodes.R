8eab8e6c7613f7fdd3db81d61363cb42d563511f
fordeia <157367221+fordeia@users.noreply.github.com>
2025-03-28 08:58:23 -0400

    Update MidtermCodes.R

--- MidtermCodes.R
+++ MidtermCodes.R
@@ -5,6 +5,9 @@ x <- c(28,29,35,37,32,26,37,39,22,29,36,38)
 qqnorm(x)
 qqline(x)
 
+#One sample t-test
+t.test(x, mu = 28, alternative = "two.sided")
+
 #Two sample t test
 Men_bodyfat <- c(13.3,6,20,8,14,19,18,25,16,24,15,1,15)
 Women_bodyfat <- c(22,16,21.7,21,30,12,23.2,28,23)

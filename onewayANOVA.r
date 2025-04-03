2a79b8e46a88ce867193caf9454199b35c19b083
fordeia <157367221+fordeia@users.noreply.github.com>
2025-04-03 10:20:06 -0400

    Update onewayANOVA.r

--- onewayANOVA.r
+++ onewayANOVA.r
@@ -20,19 +20,5 @@ one.wayBlocking <- aov(yield ~ block + factor(fertilizer), data = CropData)
 summary(one.wayBlocking)
 
 
-# Perform the two-way ANOVA
-fit <- aov(yield ~ factor(fertilizer)*factor(density), data = CropData)
 
-# Summarize the ANOVA results
-summary(fit)
-
-fit1 <- aov(yield ~ factor(fertilizer) + factor(density), data = CropData)
-
-# Summarize the ANOVA results
-summary(fit1)
-
-
-# Post-hoc test (Tukey's HSD)
-
-  TukeyHSD(fit1, conf.level=.95)
 

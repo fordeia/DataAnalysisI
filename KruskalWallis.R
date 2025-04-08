745c344c706431de3059385e6a748b92ad77213d
fordeia <157367221+fordeia@users.noreply.github.com>
2025-04-08 01:04:50 -0400

    Update KruskalWallis.R

--- KruskalWallis.R
+++ KruskalWallis.R
@@ -5,4 +5,19 @@ head(PlantGrowth)
 #Displaying group levels
 levels(PlantGrowth$group)
 
+#Reording levels
 PlantGrowth$group <- ordered(PlantGrowth$group,levels = c("ctrl", "trt1", "trt2"))
+
+#Loading the dplyr 
+library(dplyr)
+
+#Computing the summary statistics
+group_by(data, group) %>%
+  summarise(
+    count = n(),
+    mean = mean(weight, na.rm = TRUE),
+    sd = sd(weight, na.rm = TRUE),
+    median = median(weight, na.rm = TRUE),
+    IQR = IQR(weight, na.rm = TRUE)
+  )
+

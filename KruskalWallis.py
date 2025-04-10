7ea69621d02691834cf48127051b37cbd67e3b8f
fordeia <157367221+fordeia@users.noreply.github.com>
2025-04-10 09:58:10 -0400

    Update KruskalWallis.py

--- KruskalWallis.py
+++ KruskalWallis.py
@@ -1,12 +1,12 @@
 from scipy import stats
 
 # Sample data for three groups
-group1 = [2, 5, 7, 1, 9]
-group2 = [8, 3, 6, 4, 10]
-group3 = [12, 15, 11, 13, 14]
+ctr1 = [4.17, 5.58, 5.18, 6.11, 4.5, 4.61, 5.17, 4.53, 5.33, 5.14]
+trt1 = [4.81, 4.17, 4.41, 3.59, 5.87, 3.83, 6.03, 4.89, 4.32, 4.69]
+trt2 = [6.31, 5.12, 5.54, 5.5, 5.37, 5.29, 4.92, 6.15, 5.8, 5.26]
 
 # Perform the Kruskal-Wallis test
-H_statistic, p_value = stats.kruskal(group1, group2, group3)
+H_statistic, p_value = stats.kruskal(ctr1, trt1, trt2)
 
 # Print the results
 print("H statistic:", H_statistic)

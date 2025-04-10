8dc047995dae24a87a8185a9ee778577964a3c7f
fordeia <157367221+fordeia@users.noreply.github.com>
2025-04-10 10:22:29 -0400

    Update onewayANOVA.r

--- onewayANOVA.r
+++ onewayANOVA.r
@@ -1,6 +1,8 @@
 options(scipen = 999)
 #Obtaining Data
-CropData <-read.table("cropData.txt",header =TRUE,sep="\t", fill = TRUE)
+#CropData <-read.table("cropData.txt",header =TRUE,sep="\t", fill = TRUE)
+library(readxl)
+CropData <- read_excel("CropData.xlsx")
 head(CropData, 10)
 
 boxplot(CropData$yield ~ CropData$fertilizer)

a92382799213238cf25bba6dfc03e5955c553088
fordeia <157367221+fordeia@users.noreply.github.com>
2025-04-13 10:40:13 -0400

    Update Willcoxon.R

--- Willcoxon.R
+++ Willcoxon.R
@@ -1,5 +1,10 @@
 library(MASS)
+immerData<-immer
 head(immer)
+
+#Writing to an excel file. 
+library(openxlsx)
+write.xlsx(immerData, 'C:/Users/fordeia/Desktop/immerData.xlsx')
 
 # Wilcoxon signed rank test with continuity correction 
 wilcox.test(immer$Y1, immer$Y2, paired=TRUE) 

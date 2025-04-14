6b79f09129b8ab6bbdb5921cb4936020a64d22fd
fordeia <157367221+fordeia@users.noreply.github.com>
2025-04-14 05:53:27 -0400

    Update DataPrep.py

--- DataPrep.py
+++ DataPrep.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Read the excel file
-df = pd.read_excel(r"C:\Users\fordeia\DataAnalysisI\Data_Cleaning.xlsx")
+df = pd.read_excel(r"C:\Users\fordeia\Desktop\Spring2025\STT3850semII20242025\Data\Data_Cleaning.xlsx")
 
 # Print the DataFrame
 print(df)

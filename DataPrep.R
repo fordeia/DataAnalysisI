f9c0a225acd18ac2de369e0c5dc496772c0c49ba
fordeia <157367221+fordeia@users.noreply.github.com>
2025-04-13 20:51:51 -0400

    Update DataPrep.R

--- DataPrep.R
+++ DataPrep.R
@@ -2,3 +2,4 @@
 library(tidyverse)
 library(readxl)
 Data_to_clean<- read_excel("Data_Cleaning.xlsx")
+Data_to_clean<- read_excel("C:\Users\fordeia\Desktop\Spring2025\STT3850semII20242025\Data\Data_Cleaning.xlsx")

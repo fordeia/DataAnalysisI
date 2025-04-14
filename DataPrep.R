366a7e8b7a341d47c4d08fb9f5ad85ba3e7e3cee
fordeia <157367221+fordeia@users.noreply.github.com>
2025-04-14 10:24:47 -0400

    Update DataPrep.R

--- DataPrep.R
+++ DataPrep.R
@@ -3,12 +3,16 @@ library(tidyverse)
 library(readxl)
 
 Data_to_clean<- read_excel("Data_Cleaning.xlsx")
-head(Data_to_clean)
+Data_to_clean
 
 Summarizing the variables with count
 Data_to_clean %>% count(Gender)
 Data_to_clean %>% count(Age)
 Data_to_clean %>% count(Status)
+
+#Removing rows with NA
+Data_to_clean %>%
+  na.omit()
 
 #Removing rows with impossible age values
 Data_to_clean %>%

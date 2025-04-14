828fd30799c03782256fb7c1b0404e40fbb9ae5f
fordeia <157367221+fordeia@users.noreply.github.com>
2025-04-14 09:22:07 -0400

    Update DataPrep.R

--- DataPrep.R
+++ DataPrep.R
@@ -6,3 +6,5 @@ Data_to_clean<- read_excel("Data_Cleaning.xlsx")
 
 Summarizing the variables with count
 Data_to_clean %>% count(Gender)
+Data_to_clean %>% count(Age)
+Data_to_clean %>% count(Status)

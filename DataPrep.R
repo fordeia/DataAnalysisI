d90d932fece6668e1561bb19403a36e24cc413fd
fordeia <157367221+fordeia@users.noreply.github.com>
2025-04-14 09:12:44 -0400

    Update DataPrep.R

--- DataPrep.R
+++ DataPrep.R
@@ -1,8 +1,8 @@
 #Loading packages
 library(tidyverse)
 library(readxl)
+
 Data_to_clean<- read_excel("Data_Cleaning.xlsx")
 
 Summarizing the variables with count
-
- Data_to_clean %>% count(Gender)
+Data_to_clean %>% count(Gender)

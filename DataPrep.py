535bf4fc14767746c0d1be0ac17b2d5b9837b2f7
fordeia <157367221+fordeia@users.noreply.github.com>
2025-04-15 00:21:50 -0400

    Update DataPrep.py

--- DataPrep.py
+++ DataPrep.py
@@ -5,3 +5,7 @@ df = pd.read_excel(r"C:\Users\fordeia\Desktop\Spring2025\STT3850semII20242025\Da
 
 # Print the DataFrame
 print(df)
+
+# Delete rows where 'col1' is 'B'
+df = df[df['Age'] == 999]
+print(df)

ed25a97b811a417144eedaafcca33f41e9fd1d57
fordeia <157367221+fordeia@users.noreply.github.com>
2025-04-15 00:29:02 -0400

    Update DataPrep.py

--- DataPrep.py
+++ DataPrep.py
@@ -7,5 +7,5 @@ df = pd.read_excel(r"C:\Users\fordeia\Desktop\Spring2025\STT3850semII20242025\Da
 print(df)
 
 # Delete rows with impossible ages
-df.drop(df[df['Age'] == 999].index, inplace=True)
-print(df)
+#df.drop(df[df['Age'] == 999].index, inplace=True)
+#print(df)

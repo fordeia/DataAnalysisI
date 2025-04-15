20a0c4e7ad90f7658dcb95ae8bce61811c80093b
fordeia <157367221+fordeia@users.noreply.github.com>
2025-04-15 03:28:37 -0400

    Update DataPrep.R

--- DataPrep.R
+++ DataPrep.R
@@ -13,28 +13,6 @@ Data_to_clean %>% count(Gender)
 Data_to_clean %>% count(Age)
 Data_to_clean %>% count(Status)
 
-#Removing rows with NA
-Data_to_clean %>%
-  na.omit()
-
-#Remove rows with NA in specific colums
-Data_to_clean %>%
-  filter(!is.na(Status))
-
-#Removing rows with impossible age values
-Data_to_clean %>%
-  filter(Age < 900)
-
-#Removing duplicates
-Data_to_clean %>%
-  distinct()
-
-#Renaming levels
-Data_to_clean$Gender <- fct_recode(Data_to_clean$Gender, "female" = "Female", "female" ="femail")
-
-# View the result
-print(Data_to_clean)
-
 ######workflow######
 #Reloading data
 Data_to_clean<- read_excel("Data_Cleaning.xlsx")

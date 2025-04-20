f7f3fc5caa7b5bdc17990e8e6db02c51f6cf1a9a
fordeia <157367221+fordeia@users.noreply.github.com>
2025-04-20 12:22:28 -0400

    Update AdvanceTopics.R

--- AdvanceTopics.R
+++ AdvanceTopics.R
@@ -1,3 +1,6 @@
 births <- scan("http://robjhyndman.com/tsdldata/data/nybirths.dat")
 birthstimeseries <- ts(births, frequency=12, start=c(1946,1))
 birthstimeseries
+
+#Plotting the timeseries
+plot.ts(birthstimeseries)

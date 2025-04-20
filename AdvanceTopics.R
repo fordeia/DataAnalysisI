611e9ab5bf4284b7ad2487fecba4cb859f360664
fordeia <157367221+fordeia@users.noreply.github.com>
2025-04-20 13:39:38 -0400

    Update AdvanceTopics.R

--- AdvanceTopics.R
+++ AdvanceTopics.R
@@ -9,3 +9,9 @@ plot.ts(birthstimeseries)
 kings <- scan("http://robjhyndman.com/tsdldata/misc/kings.dat",skip=3)
 kingstimeseries <- ts(kings)
 plot.ts(kingstimeseries)
+
+#Smoothing the kings data, it is non-seasonal and the fluctuation are roughly the same size hence could be described by a additive model
+
+#Smoothing using a moving average of 3
+kingstimeseriesSMA3 <- SMA(kingstimeseries,n=3)
+plot.ts(kingstimeseriesSMA3)

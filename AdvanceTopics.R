c76dbf1eaf37f3aea4aee3adc5c7beef7591b621
fordeia <157367221+fordeia@users.noreply.github.com>
2025-04-22 00:20:52 -0400

    Update AdvanceTopics.R

--- AdvanceTopics.R
+++ AdvanceTopics.R
@@ -46,3 +46,7 @@ rain <- scan("http://robjhyndman.com/tsdldata/hurst/precip1.dat",skip=1)
 rainseries <- ts(rain,start=c(1813))
 plot.ts(rainseries)
 
+#Simple Exponential Smoothing
+rainseriesforecasts <- HoltWinters(rainseries, beta=FALSE, gamma=FALSE)
+rainseriesforecasts
+

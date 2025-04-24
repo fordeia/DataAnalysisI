5886c719a1c2f3e4ba93524127ee8dc82b022f56
fordeia <157367221+fordeia@users.noreply.github.com>
2025-04-24 09:53:54 -0400

    Update AdvanceTopics.R

--- AdvanceTopics.R
+++ AdvanceTopics.R
@@ -75,7 +75,7 @@ rainseriesforecasts2
 plot(rainseriesforecasts2)
 
 #Correlogram to test for auto-correlation
-acf(x, na.action = na.pass)
+acf(rainseriesforecasts2, na.action = na.pass)
 acf(rainseriesforecasts2$residuals, lag.max=20)
 
 #Box test to test for auto-correlation

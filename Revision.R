145449afc1ed0c9b2b9d56214cd470ff9c380835
fordeia <157367221+fordeia@users.noreply.github.com>
2025-04-28 09:49:57 -0400

    Update Revision.R

--- Revision.R
+++ Revision.R
@@ -2,3 +2,10 @@
 install.packages("pacman")
 library(pacman)
 library(tidyverse)
+
+#Importing the data
+retail <- read_csv("https://raw.githubusercontent.com/PacktPublishing/Forecasting-Time-Series-Data-with-Facebook-Prophet/main/data/online_retail.csv")
+
+#Ploting the time series 
+retailtimeseries <- ts(retail)
+plot.ts(retailtimeseries)

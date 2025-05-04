0556675f68a9d22813b274f94018e2f786d963d0
fordeia <157367221+fordeia@users.noreply.github.com>
2025-05-04 15:54:19 -0400

    Update finalExSpring.R

--- finalExSpring.R
+++ finalExSpring.R
@@ -19,3 +19,10 @@ fviz_eig(pca_result, addlabels = TRUE)
 #Biplot to display the relationship between the PC and independent variables. 
 fviz_pca_var(pca_result, col.var = "black")
 
+#Extracting components that explains 95% of the variability
+explained_variance <- pca_result$sdev^2 / sum(pca_result$sdev^2)
+cumulative_variance <- cumsum(explained_variance)
+n_components <- which(cumulative_variance >= 0.95)[1] # Keep until 95% explained variance
+X_pca <- as.data.frame(pca_result$x[, 1:n_components])
+head(X_pca)
+

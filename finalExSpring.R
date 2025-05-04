8636fe3bef8b48dabcf3f7c49d24faf460948198
fordeia <157367221+fordeia@users.noreply.github.com>
2025-05-04 14:54:10 -0400

    Update finalExSpring.R

--- finalExSpring.R
+++ finalExSpring.R
@@ -1,2 +1,8 @@
+#Question 3 ==============================
+#Down loading the mtcars dataset
 data("mtcars")
 head(mtcars)
+
+#The principal component analysis
+pca_result <- prcomp(mtcars[, -1], scale = TRUE, center = TRUE) # Don't include outcome in PCA
+summary(pca_result)

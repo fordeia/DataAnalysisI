31ac82454a7223064cf0d98b28ba199492f58b46
fordeia <157367221+fordeia@users.noreply.github.com>
2025-11-25 10:32:10 -0500

    Update AdvanceTopics.py

--- AdvanceTopics.py
+++ AdvanceTopics.py
@@ -2,26 +2,39 @@
 # 1. IMPORT LIBRARIES
 ########################################
 import pandas as pd
+import matplotlib.pyplot as plt
 import requests
 from io import StringIO
 
 ########################################
-# 2. DEFINE DATA URL AND HEADERS
+# 2. DOWNLOAD BIRTHS DATA
 ########################################
 url = "http://robjhyndman.com/tsdldata/data/nybirths.dat"
 headers = {'User-Agent': 'Mozilla/5.0'}
-
-########################################
-# 3. DOWNLOAD DATA FROM URL
-########################################
 response = requests.get(url, headers=headers)
 
-########################################
-# 4. CHECK RESPONSE AND LOAD DATA
-########################################
 if response.status_code == 200:
     data = StringIO(response.text)
-    births = pd.read_csv(data, header=None).squeeze()
-    print(births)
+    births = pd.read_csv(data, header=None).squeeze()  # now births exists
+    print("Data loaded successfully")
 else:
-    print("Download failed:", response.status_code)
+    raise Exception("Download failed:", response.status_code)
+
+########################################
+# 3. CREATE TIME SERIES
+########################################
+birthstimeseries = pd.Series(
+    births.values,
+    index=pd.date_range(start='1946-01', periods=len(births), freq='ME')  # month-end frequency
+)
+print(birthstimeseries.head())
+
+########################################
+# 4. PLOT TIME SERIES
+########################################
+plt.figure(figsize=(10,4))
+plt.plot(birthstimeseries)
+plt.title("Monthly Births in New York (1946 onwards)")
+plt.xlabel("Year")
+plt.ylabel("Number of Births")
+plt.show()

5b06156d0f672589bad63a49c6338f9d288fd7cf
Jesus <sisniega.serrano@gmail.com>
2025-12-02 00:48:08 -0500

    Fix typo in contingency table variable name

--- ChisqInd.R
+++ ChisqInd.R
@@ -1,23 +1,24 @@
 #Recreating the contingency table in R:
 
-Contintable=matrix(c(50,125,90,45,75,175,30,10),ncol=2) 
-colnames(Contintable)=c("Snacks","No Snacks") 
-rownames(Contintable)=c("Action","Comedy","Family", "Horror")
-print(Contintable)
+Contingtable = matrix(c(50,125,90,45,75,175,30,10),ncol=2) 
+colnames(Contingtable) = c("Snacks","No Snacks") 
+rownames(Contingtable) = c("Action","Comedy","Family", "Horror")
+print(Contingtable)
 
 #Visualizing the data with a balloonplot
 install.packages("gplots")
 library("gplots") 
 #convert the data as a table 
-dt <- as.table(as.matrix(Contintable)) 
+dt <- as.table(as.matrix(Contingtable)) 
 #Graph 
 balloonplot(t(dt), main ="Purchase", xlab ="", ylab="", label = FALSE, show.margins = FALSE) 
 
 #Chi-square test
-chisq<-chisq.test(Contintable) 
+chisq<-chisq.test(Contingtable) 
 chisq
 
 #Expected values 
 chisq$expected
+
 
 
